652653166ced70a1047eb9f74d5b37eb0085fe50
Edgar Vergara <edgarvm13@hotmail.com>
2024-02-13 11:53:37 +0100

    feat: nexus appbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:nexus/providers/app_state.dart';
 import 'package:nexus/screens/bar_screen.dart';
 import 'package:nexus/screens/games_screen.dart';
 import 'package:nexus/screens/lan_room_screen.dart';
+import 'package:nexus/widgets/nexus_app_bar.dart';
 import 'package:nexus/widgets/nexus_bottom_nav_bat.dart';
 import 'package:provider/provider.dart';
 
@@ -27,12 +28,11 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'Material App',
+        debugShowCheckedModeBanner: false,
         home: Consumer<AppState>(
           builder: (context, appState, child) {
             return Scaffold(
-              appBar: AppBar(
-                title: const Text('Material App Bar'),
-              ),
+              appBar: const NexusAppBar(),
               body: screens[appState.selectedScreen],
               bottomNavigationBar: const NexusBottomNavBar(),
             );
---
+++ lib/widgets/nexus_app_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class NexusAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const NexusAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: const Icon(Icons.person),
+        tooltip: 'Sign in',
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.timer),
+          tooltip: 'Top up hours',
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
